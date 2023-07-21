1e70406613b5b27fa4ac4dd6222234ca543b45a1
Amartya Kumar <amartya324@gmail.com>
2023-07-22 00:47:05 +0530

    first commit

---
+++ src/Sidebar.jsx
@@ -0,0 +1,40 @@
+import { FaTimes } from "react-icons/fa";
+import { useGlobalContext } from "./context";
+import { links } from "./data";
+const Sidebar = () => {
+  const { isSidebarOpen, closeSidebar } = useGlobalContext();
+  return (
+    <aside className={isSidebarOpen ? "sidebar show-sidebar" : "sidebar"}>
+      <div className="sidebar-header">
+        <h2>Amartya Kumar</h2>
+        <button className="close-btn" onClick={closeSidebar}>
+          <FaTimes />
+        </button>
+      </div>
+      <ul className="links">
+        {links.map((link) => {
+          const { id, url, text, icon } = link;
+          return (
+            <li key={id}>
+              <a href={url}>
+                {icon} {text}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <ul className="social-icons">
+        {links.map((link) => {
+          const { id, url, icon } = link;
+          return (
+            <li key={id}>
+              <a href={url}>{icon}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </aside>
+  );
+};
+
+export default Sidebar;
